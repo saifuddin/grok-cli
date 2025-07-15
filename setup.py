75e5cbafe6ea8e88d11b41820753f9d9642d4a3e
Saifuddin Fakhri <saifuddin.fakhri@gmail.com>
2025-07-15 18:30:33 -0500

    Fix: include grokcli.commands in package discovery

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="grokcli",
     version="0.1.0",
-    packages=find_packages(exclude=["tests", "docs"]),
+    packages=find_packages(include=["grokcli", "grokcli.*"], exclude=["tests", "docs"]),
     install_requires=[
         "requests",
         "pyyaml",
